131f8ab2d0f7086f5ae6e6fbc584439ed4b07444
Igor Kuzmenkov <me@igorduino.ru>
2023-03-19 10:37:05 +0300

    init for task

--- settings.py
+++ settings.py
@@ -9,6 +9,9 @@ class Settings:
 
     DB_URL = "sqlite:///database.db"
 
+    OXYGYN_PRICE = config("OXYGYN_PRICE", cast=int, default=7)
+    FUEL_PRICE = config("FUEL_PRICE", cast=int, default=10)
+
     SECRET_KEY = config(
         "SECRET_KEY",
         default="".join([random.choice(string.ascii_letters) for _ in range(32)]),
