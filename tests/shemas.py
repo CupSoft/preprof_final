b31dcfe5e21f6c7ae6f6e66ba6e99973df6211f7
Deformater <muravenkog@yandex.ru>
2023-03-19 12:53:22 +0300

    add unittests

---
+++ tests/shemas.py
@@ -0,0 +1,24 @@
+from pydantic import BaseModel
+
+
+class DayPD(BaseModel):
+    num: int | None
+    current_sh: int | None
+    current_power: int | None
+    current_oxi: int | None
+    use_power: int | None
+    use_oxi: int | None
+    distribution_engine: int | None
+    distribution_autoclave: int | None
+
+
+class PointPD(BaseModel):
+    num: int | None
+    day: int | None
+    current_power: int | None
+    current_oxi: int | None
+
+
+class AlgoResultPD(BaseModel):
+    days: list[DayPD]
+    points: list[PointPD]
